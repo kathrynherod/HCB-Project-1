841cd0b9db42fdb46c78df5809ae10d1ba79535a
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-07-31 20:52:55 -0500

    fixed broken links

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
  jQuery(document).ready(function($) {
  
     $(".scroll a, .navbar-brand, .gototop").click(function(event){   
-    event.preventDefault();
+    
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600,'swing');
     $(".scroll li").removeClass('active');
     $(this).parents('li').toggleClass('active');
