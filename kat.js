764502b4a856d314410678f456d6ac872b6ba9d5
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-08-03 01:06:32 -0500

    testing login

--- kat.js
+++ kat.js
@@ -1,45 +1,12 @@
-/*
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+$("#login-btn").on("click", function() {
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    // User is signed out.
-    // ...
-  }
-});
+	var email = $("#login-email").val().trim();
+	var password = $("#login-pw").val().trim();
 
-  function writeUserData(userName, email, password, firstName, lastName, age, zipCode) {
-        firebase.database().ref('users/' + userId).set({
-            UserName: userName,
-            Email: email,
-            Password: password,
-            FirstName: firstName,
-            LastName: lastName,
-            Age: age,
-            ZipCode: zipCode
-        });
-    }
-
-
-
-
-*/
-
- //send that input to our database
-  
-
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+})
