0531ddfe7ab54ad5b49f65ff16d6a82ccb67f69c
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-08-02 22:55:51 -0500

    added comments to phil's js file and added username collection

--- phil.js
+++ phil.js
@@ -1,44 +1,51 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyDOjDpYcoEnBzdA5y3d6EPrRcmMzIq5aBc",
     authDomain: "groupproject1-4b4d1.firebaseapp.com",
     databaseURL: "https://groupproject1-4b4d1.firebaseio.com",
     projectId: "groupproject1-4b4d1",
     storageBucket: "groupproject1-4b4d1.appspot.com",
     messagingSenderId: "345420353976"
-  };
-
+};
+//this uses the info in config and passes into the function calling firebase
 firebase.initializeApp(config);
+//rename firebase to database
 var database = firebase.database();
-  var userName = "";
-  var email = "";
-  var password = "";
-  var firstName = "";
-  var lastName = "";
-  var age = "";
-  var zipCode = "";
 
 
+//global variables
+var userName = "";
+var email = "";
+var password = "";
+var firstName = "";
+var lastName = "";
+var age = "";
+var zipCode = "";
+
+//when user clicks register button
 $("#register-btn").on("click", function() {
-  event.preventDefault();
 
+    //stop page from refreshing
+    event.preventDefault();
 
-  email = $("#user-email").val().trim();
-  password = $("#user-pw").val().trim();
-firstName = $("#user-first").val().trim();
- lastName = $("#user-last").val().trim();
-  age = $("#user-age").val().trim();
-  zipcode = $("#user-zip").val().trim();
+    //grab the user's input from the page
+    userName = $("#user-name").val().trim();
+    email = $("#user-email").val().trim();
+    password = $("#user-pw").val().trim();
+    firstName = $("#user-first").val().trim();
+    lastName = $("#user-last").val().trim();
+    age = $("#user-age").val().trim();
+    zipcode = $("#user-zip").val().trim();
 
-            database.ref().push({
+    //send that input to our database
+    database.ref().push({
 
-                UserName: userName,
-                Email: email,
-                Password:  password,
-                FirstName: firstName,
-                LastName: lastName,
-                Age: age,
-                ZipCode: zipCode
-            });
-}
-)
+        UserName: userName,
+        Email: email,
+        Password: password,
+        FirstName: firstName,
+        LastName: lastName,
+        Age: age,
+        ZipCode: zipCode
+    });
+})
