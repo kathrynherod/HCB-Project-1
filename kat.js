18f04bb9b0fff6a9f5540a48148fdc7ef35ee063
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-08-09 12:49:27 -0500

    fb

--- kat.js
+++ kat.js
@@ -91,33 +91,9 @@ var manageUsers = {
     },
     facebookAPI: function() {
 
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId: '330846097371920',
-                autoLogAppEvents: true,
-                xfbml: true,
-                version: 'v2.10'
-            });
-            FB.AppEvents.logPageView();
-        };
+        
 
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=330846097371920";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        
     },
     userPromise: function(email, password) {
 
